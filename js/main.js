9a9ac28419ac54a6abff88847c7bdb5a61c02cda
João Antônio <56412536+joao-dev1@users.noreply.github.com>
2022-11-16 18:10:50 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 
 function pokemondle(){
+
 var input_namePokemon = document.getElementById('name_pokemon');
-pokemondle_SearchForName(input_namePokemon.value);
+console.log(input_namePokemon.value);
+pokemondle_SearchForName(input_namePokemon.value.toString());
+
 }
 function pokemondle_SearchForName(name_pokemon){
    fetch(`https://pokeapi.co/api/v2/pokemon/${name_pokemon}`)
