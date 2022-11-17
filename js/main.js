e792211578ebc139202b56c68d5276c8b486a306
João Antônio <56412536+joao-dev1@users.noreply.github.com>
2022-11-17 13:43:04 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,10 @@
 import {clearWhiteSpacesAndToLower} from "utils.js";
-function pokemondle(){
 
-var input_namePokemon = document.getElementById('name_pokemon');
 
-pokemondle_SearchForName(input_namePokemon.value.toString());
+async function pokemondle_SearchForName(name_pokemon){
 
-}
-function pokemondle_SearchForName(name_pokemon){
-    
-    name_pokemon = clearWhiteSpacesAndToLower(name_pokemon);
+    name_pokemon = await clearWhiteSpacesAndToLower(name_pokemon);
+
    console.log(name_pokemon);
    fetch(`https://pokeapi.co/api/v2/pokemon/${name_pokemon}`)
     .then(T => T.json())
@@ -20,4 +16,12 @@ function pokemondle_SearchForName(name_pokemon){
         img_namePokemon.src = j.sprites.front_default;
      
     })
+}
+
+function pokemondle(){
+
+    var input_namePokemon = document.getElementById('name_pokemon');
+    
+    pokemondle_SearchForName(input_namePokemon.value.toString());
+    
 }
